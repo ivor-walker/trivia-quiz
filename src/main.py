f2bf486cf6028a7fb944da021880e8c94331e6e3
Ivor Walker <ivorivor1@hotmail.com>
2025-02-11 13:42:30 +0000

    Implemented basic 3

--- src/main.py
+++ src/main.py
@@ -51,6 +51,26 @@ def user_incorrect():
     # Display a message
     print("Incorrect! You have answered " + str(incorrect_answers) + " questions incorrectly.");
 
+    check_game_over();
+
+"""
+Handle game over
+@param max_incorrect_answers: Maximum number of incorrect answers before game over
+"""
+
+def check_game_over(
+        max_incorrect_answers = 3
+    ):
+    # Make counter variables global
+    global correct_answers;
+    global incorrect_answers;
+
+    # Check if user has exceeded the maximum number of incorrect answers
+    if incorrect_answers >= max_incorrect_answers:
+        print("Game over! You have answered " + str(incorrect_answers) + " questions incorrectly.");
+        print("You have answered " + str(correct_answers) + " questions correctly.");
+        exit();
+
 # Get questions
 questions = Questions();
 
